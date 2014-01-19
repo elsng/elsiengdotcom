53a9d6a2a168a2a50fb75fd7d78c643479af5679
Elsie Ng <elsie.ng.design@gmail.com>
2014-01-19 16:42:10 +0000

    Now parsing JSON

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,11 +1,86 @@
 package controllers
 
-import "github.com/robfig/revel"
+import (
+	"fmt"
+	"net/http"
+	"encoding/json"
+	"io/ioutil"
+	"github.com/robfig/revel"
+)
+
+
+type Elsie struct {
+    Elsieng Elsieng_info
+}
+
+type Elsieng_info struct {
+    Profile  Profile_info
+    Project []Project_info
+}
+
+type Project_info struct {
+    Id	int
+    Name	string
+    Gist	string
+    Picture	string
+    Partner	string
+    Style	string
+    Link	string
+    About	string
+    Role	string
+    Section []Section_info
+}
+
+type Profile_info struct {
+    Name    string
+    Gist    string
+    Current    string
+}
+
+type Section_info struct {
+	Header	string
+	Text	string
+	Image	string
+}
+
+func perror(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
 
 type App struct {
 	*revel.Controller
 }
 
 func (c App) Index() revel.Result {
-	return c.Render()
+	url := "http://elsieng.com/sample.json"
+
+    res, err := http.Get(url)
+    perror(err)
+    defer res.Body.Close()
+    body, err := ioutil.ReadAll(res.Body)
+    perror(err)
+
+    var data Elsie
+    err = json.Unmarshal(body, &data)
+    if err != nil {
+        fmt.Printf("%T\n%s\n%#v\n",err, err, err)
+        switch v := err.(type){
+            case *json.SyntaxError:
+                fmt.Println(string(body[v.Offset-40:v.Offset]))
+        }
+    }
+    
+    profile := data.Elsieng.Profile
+    
+    for i, project := range data.Elsieng.Project{
+        
+        fmt.Println(i, project.Name, project.Gist)
+        
+        for j, section := range project.Section{
+        	fmt.Println(j, section.Text)
+        }
+    }
+	return c.Render(profile.Name)
 }
