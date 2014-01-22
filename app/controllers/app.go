6274413b2004de0bd4080d4ed6cf4c67fa0f57c2
elsng <awesome@elsieng.com>
2014-01-22 03:07:20 +0000

    Better objects and reversed list. Basically you can add whatever projects to the end of the json file and they'll always show up at the top.

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -51,6 +51,9 @@ type Section_info struct {
 	Header	string
 	Text	string
 	Image	string
+	Href	string
+	Hreftext	string
+	Align	string
 }
 
 func perror(err error) {
@@ -61,6 +64,32 @@ func perror(err error) {
 
 type App struct {
 	*revel.Controller
+}
+
+type elsieProfile struct {
+	Name    string
+    Gist    string
+    Location string
+    Current    string
+    Available	bool
+    Twitter	string
+    Github	string
+    Linkedin	string
+    Dribbble	string
+    Steam	string
+}
+
+type projectList struct {
+	Project []projectItem
+}
+
+type projectItem struct {
+	Id	int
+	Name	string
+	Gist	string
+	Picture	string
+	Brand	string
+	Button	string
 }
 
 func (c App) Index() revel.Result {
@@ -82,16 +111,47 @@ func (c App) Index() revel.Result {
         }
     }
     
-    //for i, project := range data.Elsieng.Project{	
-        //for j, section := range project.Section{
-        	//fmt.Println(j, section.Text)
-        //}
-    //}
-    return c.Render(data)
+    //Profile
+    myProfile := elsieProfile{}
+    
+	myProfile.Name = data.Elsieng.Profile.Name
+	myProfile.Gist = data.Elsieng.Profile.Gist
+	myProfile.Location = data.Elsieng.Profile.Location
+	myProfile.Current = data.Elsieng.Profile.Current
+	myProfile.Available = data.Elsieng.Profile.Available
+	myProfile.Twitter = data.Elsieng.Profile.Twitter
+	myProfile.Github = data.Elsieng.Profile.Github
+	myProfile.Linkedin = data.Elsieng.Profile.Linkedin
+	myProfile.Dribbble = data.Elsieng.Profile.Dribbble
+	myProfile.Steam = data.Elsieng.Profile.Steam
+    
+    //Project List
+    thelist :=  make([]projectItem, 0)
+    
+    for i := len(data.Elsieng.Project)-1; i >= 0; i-- {
+    	for j, project := range data.Elsieng.Project{
+    		if i == project.Id{	
+        		projectItem := projectItem{}
+				projectItem.Id = project.Id
+				projectItem.Name = project.Name
+				projectItem.Gist = project.Gist
+				projectItem.Picture = project.Picture
+				projectItem.Brand = project.Brand
+				projectItem.Button = project.Button
+				thelist = append(thelist, projectItem)
+				j--
+			}
+        }
+    }
+    return c.Render(myProfile,thelist)
 }
 
+//Project Template
+
 type currentProject struct{
+	Id	int
 	Name	string
+	Brand	string
 	Catchphrase	string
 	Partner	string
 	Link	string
@@ -101,9 +161,13 @@ type currentProject struct{
 }
 
 type projectSection struct{
+	Id	int
 	Header	string
 	Text	string
 	Image	string
+	Href	string
+	Hreftext	string
+	Align	string
 }
 
 func (c App) Project(id int) revel.Result {
@@ -130,14 +194,26 @@ func (c App) Project(id int) revel.Result {
     for i, project := range data.Elsieng.Project{
     	
         if (i == id){
+        	cp.Id = project.Id
         	cp.Name = project.Name
+        	cp.Brand = project.Brand
         	cp.Catchphrase = project.Catchphrase
+        	cp.Partner = project.Partner
+        	cp.Link = project.Link
+        	cp.Gettext = project.Gettext
+        	cp.About = project.About
         	
         	cpsections := make([]projectSection, 0)
         	
         	for j, section := range project.Section{
         		cpsection := projectSection{}
+        		cpsection.Id = j
         		cpsection.Header = section.Header
+        		cpsection.Text = section.Text
+        		cpsection.Image = section.Image
+        		cpsection.Href = section.Href
+        		cpsection.Hreftext = section.Hreftext
+        		cpsection.Align = section.Align
         		cpsections = append(cpsections, cpsection)
         		j++
 			}
